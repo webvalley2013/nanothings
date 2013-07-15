581f28d0a9e78e2afd2ee412ddb1cf92c984bfc1
Gianmarco Silli <silli.gianmarco@libero.it>
2013-07-15 10:11:50 +0200

    corrected process/urls.py

--- process/urls.py
+++ process/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('process.views',
     (r'list/$', 'process_list'),
     (r'run/process_test/(\d+)/(\d+)/$', 'run_process_test'),
     (r'run/process_get/$', 'run_process_get'),
-    (r'run/process_post/$', 'run_process_post')
+    (r'run/process_post/$', 'run_process_post'),
 #   (r'run/3danlysis/(\d+)/(\d+)/$', 'run_process_test'),
 #   (r'run/plrfunc/(\d+)/(\d+)/$', 'run_process_test'),
 #   (r'run/hadoop/(\d+)/(\d+)/$', 'run_process_test'),
