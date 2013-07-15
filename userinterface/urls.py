fbd9e8f42d5626fdfcc3132e0c48acb139a5082b
Ossama Edbali <ossedb@gmail.com>
2013-07-15 10:55:22 +0200

    added css file

---
+++ userinterface/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+from userinterface import views
+
+urlpatterns = patterns("",
+    url(r'process_list/$', views.list_of_processes, name="list_of_processes"),
+    url(r'process/(\d+)/$', views.detail, name="detail"),
+)
