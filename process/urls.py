3b3119cb116fa2e57e897e280f0522ae70ecb58e
droghetti <arthik88@gmail.com>
2013-07-18 03:13:07 +0200

    first implementation plr process

--- process/urls.py
+++ process/urls.py
@@ -19,6 +19,7 @@ from process import views
 urlpatterns = patterns('process.views',
     url(r'^list/$', 'process_list'),
     url(r'run/test_int/(\d+)$', 'run_test_int'),
+    url(r'run/test_plr/(\d+)$', 'run_test_plr'),
     url(r'run/process/3d/(\d+)$', 'run_process_3d', name='run3d'),
     url(r'status/(\d+)$', 'status', name="status"),
     url(r'detail/(\d+)$', 'detail', name="detail"),
--- process/views.py
+++ process/views.py
@@ -1,21 +1,8 @@
-# This file is part of nanothings.
-#
-#     nanothings is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU Affero GPL as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
-#
-#     Foobar is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU Affero GPL for more details.
-#
-#     You should have received a copy of the GNU Affero GPL
-#     along with nanothings.  If not, see <http://www.gnu.org/licenses/>.
-# MODULES
+from django.db.utils import DatabaseError
 from django.views.decorators.csrf import csrf_exempt
 from .models import Process, RunningProcess
 from django.http import HttpResponse
+from django.db import connection, transaction
 from django.shortcuts import render, get_object_or_404
 import json
 import datetime
@@ -169,6 +156,57 @@ def run_test_int(request, p_id):
                 'success': True,
                 'polling_url': '/process/status/' + str(p.pk)
             }
+
+    else:
+        return {
+                   'success': False,
+                   'message': 'input parameters were invalid'
+               }, 400
+
+
+@ajax()
+@csrf_exempt
+def run_test_plr(request, p_id):
+    try:
+        proc = Process.objects.get(pk=p_id)
+    except Process.DoesNotExist:
+        return {'success': False,
+                'message': 'process with id {0} does not exists'.format(p_id)}, 400
+
+    if proc.type != 'plr':
+        return {'success': False,
+                'message': 'process with id {0} is not a 3d analisys'.format(p_id)}, 400
+
+    # If the parameters are correct:
+    ProcessForm = FormFactory(proc).build_form()
+    form = ProcessForm(request.POST)
+    if form.is_valid():
+        parameters = form.save()
+        from .tasks import process_plr
+
+        # Add task to broker code
+        # try:
+        task = process_plr.delay(parameters["url_pathways"], parameters["url_data"], parameters["sel_pathways"], parameters["thr"])
+        # except Exception as e:
+        #     return {
+        #                'success': False,
+        #                'message': 'Internal server error ' + e.message
+        #            }, 500
+        # else:
+
+            # Save running process to db
+        p = RunningProcess()
+        p.process_type = proc  # (3d, hadoop, R/PLR, ...)
+        p.task_id = task.id
+        p.started = datetime.datetime.now()
+        p.inputs = json.dumps(parameters)
+        p.save() # Save the running process to the DB
+
+        # Return response to the client.
+        return {
+            'success': True,
+            'polling_url': '/process/status/' + str(p.pk)
+        }
 
     else:
         return {
